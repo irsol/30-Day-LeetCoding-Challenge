efbd7499a0c630c0af05ddb92fbef2110983b54a
irsol <irinywka@gmail.com>
2020-04-01 17:47:43 +0200

    Added xolution to Single Number problem

--- Single Number.py
+++ Single Number.py
@@ -10,3 +10,18 @@ Input: [2,2,1]
 Output: 1
 """
 
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        s = set()
+        for num in nums:
+          if num in s:
+              s.remove(num)
+          else:
+              s.add(num)
+        return s.pop()
