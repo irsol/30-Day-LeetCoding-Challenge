6ad698cec79e526c5ba8d2b783bced55f5407312
irsol <irinywka@gmail.com>
2020-04-01 17:47:00 +0200

    Added description

---
+++ Single Number.py
@@ -0,0 +1,12 @@
+# Day 1: Single Number
+
+# Given a non-empty array of integers, every element appears twice except for one.
+# Find that single one.
+
+"""
+Example 1:
+
+Input: [2,2,1]
+Output: 1
+"""
+
