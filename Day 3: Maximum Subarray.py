00112658c493cf264ce7f284685b0df55c603847
irsol <irinywka@gmail.com>
2020-04-03 18:18:15 +0200

    Added solution to day 3

--- Day 3: Maximum Subarray.py
+++ Day 3: Maximum Subarray.py
@@ -8,3 +8,34 @@
 # Input: [-2,1,-3,4,-1,2,1,-5,4],
 # Output: 6
 # Explanation: [4,-1,2,1] has the largest sum = 6.
+
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        if len(nums) == 0:
+            return 0
+        t = 0
+        final_max = nums[0]
+        for i in range(0, len(nums)):
+            t = max(nums[i], nums[i]+t)
+            if t > final_max:
+                final_max = t
+        return final_max
+
+
+
+#        first_max_sum = 0
+#        final_max_sum = 0
+
+#        for n in range(0, len(nums)):
+#            first_max_sum = first_max_sum + nums[n]
+            #if first_max_sum < 0:
+#                first_max_sum = 0
+#            if final_max_sum < first_max_sum:
+#                final_max_sum = first_max_sum
+#        return final_max_sum
