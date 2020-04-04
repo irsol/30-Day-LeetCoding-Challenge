43e1e80128bfaaec3abec4bca778f67d597e8d74
irsol <irinywka@gmail.com>
2020-04-04 15:06:45 +0200

    Added description

---
+++ Day 4: Move Zeroes.py
@@ -0,0 +1,17 @@
+# Day 4: Move Zeroes
+#
+# Given an array nums, write a function to move all 0's to the end of it while maintaining the
+# relative order of the non-zero elements.
+#
+# Example:
+#
+# Input: [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
