0a2fa94ea79ca2523619ed6b0d19fe9cb8310c6c
irsol <irinywka@gmail.com>
2020-04-04 17:13:03 +0200

    Added unittests

--- Day 4: Move Zeroes.py
+++ Day 4: Move Zeroes.py
@@ -1,3 +1,4 @@
+import unittest
 # Day 4: Move Zeroes
 #
 # Given an array nums, write a function to move all 0's to the end of it while maintaining the
@@ -8,6 +9,20 @@
 # Input: [0,1,0,3,12]
 # Output: [1,3,12,0,0]
 
+class unitest(unittest.TestCase):
+    def testEmptylist(self):
+        input = []
+        output = []
+        self.assertEqual(Solution().moveZeroes(input),output)
+    def test(self):
+        input = [1, 0, 0, 3, 11, 55]
+        output = [1, 3, 11, 55, 0, 0]
+        self.assertEqual(Solution().moveZeroes(input),output)
+    def testZero(self):
+        input = [0, 0, 1]
+        output = [1, 0, 0]
+        self.assertEqual(Solution().moveZeroes(input), output)
+        
 
 class Solution(object):
     def moveZeroes(self, nums):
@@ -22,3 +37,7 @@ class Solution(object):
                 nums[i], nums[count_elem] = nums[count_elem], nums[i]   # swap the positions of the elements
                 count_elem += 1
         return nums
+
+
+if __name__ == '__main__':
+    unittest.main()
