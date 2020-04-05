ee1044c27e15ebcdaa540b41579d48061c14a5d2
irsol <irinywka@gmail.com>
2020-04-05 10:22:42 +0200

    Added description

---
+++ Day 5: Best Time to Buy and Sell Stock2.py
@@ -0,0 +1,14 @@
+# Day 5: Best Time to Buy and Sell Stock 2
+#
+# Say you have an array for which the ith element is the price of a given stock on day i.
+#
+# Design an algorithm to find the maximum profit. You may complete as many transactions as you 
+# like (i.e., buy one and sell one share of the stock multiple times).
+#
+# Example 1:
+#
+# Input: [7,1,5,3,6,4]
+# Output: 7
+# Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+            # Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+            
