be10e20c8fc4104d2c00d87b3843c223da21dcd0
irsol <irinywka@gmail.com>
2020-04-06 18:30:49 +0200

    Added solution

--- Day 6: Group Anagrams.py
+++ Day 6: Group Anagrams.py
@@ -15,3 +15,23 @@
 # Note:
 # All inputs will be in lowercase.
 # The order of your output does not matter.
+
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        anagram = {}
+
+        for s in strs:
+            t = "".join(sorted(s))
+            if t in anagram:
+                anagram[t].append(s)
+            else:
+                anagram[t] = [s]
+        return list(anagram.values())
+ 
+
+
