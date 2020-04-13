12c361651cfacc980909d32da5fe2f6b7b885e97
irsol <irinywka@gmail.com>
2020-04-13 17:00:00 +0200

    Added description

---
+++ Day 13: Contiguous Array.py
@@ -0,0 +1,11 @@
+# Contiguous Array
+# 
+# Given a binary array, find the maximum length of a contiguous subarray with equal number of
+# 0 and 1.
+#
+# Example 1:
+# Input: [0,1]
+# Output: 2
+# Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
+
+
